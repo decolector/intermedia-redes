0462cf34592e53540ccde501717b12db85c86e1b
Camilo Martinez <cmart@decolector.net>
2016-11-14 11:37:47 -0500

    comments

--- code/talleres/google_forms_web/server.py
+++ code/talleres/google_forms_web/server.py
@@ -1,3 +1,14 @@
+#!/usr/bin/Python
+
+#Servidor temboo web
+#Este es un simple servidor que hace de proxy o relevo
+#entre una pagina web que usa el sdk de javascript de temboo
+#y el servidor de temboo.
+# Basado en un ejemplo de temboo.com
+# Camilo Martinez
+# cmart@decolector.net
+
+
 from twisted.web.server import Site
 from twisted.web.resource import Resource
 from twisted.internet import reactor
@@ -7,8 +18,10 @@ from temboo.Library.Google.Sheets import GetValues
 from temboo.core.session import TembooSession
 from temboo.core.proxy import TembooProxy
 
+
 class ProxyServer(Resource):
     def render_POST(self, request):
+        print("Enviando petición a temboo")
         # Create a session with your Temboo account details
         session = TembooSession('almanaranja', 'MyFirstApp', '1LB6hWkQNaFcBYD7UhRsz2AxlRth6GUS')
 
@@ -40,12 +53,22 @@ class ProxyServer(Resource):
         return result
 
 
-
+#root representa la raiz del servidor o "/"
+#este es el recurso principal a servir
 root = Resource()
+#agretamos el archivo index.html al recurso
 root.putChild('', File('index.html'))
+#Servimos el folder con ls sctips de javascript
 root.putChild('js', File('js/'))
+#en la direccion /proxy-server esta el endpoint o
+#recurso que se encarga de hacer la peticion a temboo.com
 root.putChild('proxy-server', ProxyServer())
 
 factory = Site(root)
+#Se inicia el servidor en el puerto 8000
 reactor.listenTCP(8000, factory)
+print(
+"Iniciando servidor en  \n"
++"Puede ver la página en la url: http://127.0.0.1:8000"
+)
 reactor.run()
